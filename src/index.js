52e47a1b576180d4df86917a6e15e190b8ede746
Flober <andrewchen14250@gmail.com>
2021-02-10 22:42:45 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./index.scss"
+
+export const redDiv = (props) => {
+    return (<div className="red-div">{props.children}</div>)
+}
+
+export const greenDiv = (props) => {
+    return (<div className="green-div">{props.children}</div>)
+}
